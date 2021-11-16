e58f665f8b583fec1656f6e67a9d5f83f7e70d35
francop <franco.pradelli94@gmail.com>
2021-11-16 17:46:16 +0100

    Modified definition of parameters

--- mocafe/fenut/parameters.py
+++ mocafe/fenut/parameters.py
@@ -3,16 +3,30 @@ import pathlib
 from pandas_ods_reader import read_ods
 
 
+def from_dict(parameters: dict):
+    param_df = pd.DataFrame({
+        "name": parameters.keys(),
+        "sim_value": [parameters[param] for param in parameters]
+    })
+    return Parameters(param_df)
+
+
+def from_ods_sheet(file: pathlib.Path, sheet: str):
+    # check if file exists
+    if not file.exists():
+        raise RuntimeError(f"Parameters file {str(file)} does not exist.")
+    # check suffix
+    if file.suffix != ".ods":
+        raise NotImplementedError(f"Detected file with suffix {file.suffix}. Only .ods files are supported.")
+    # import ods file
+    param_df: pd.DataFrame = read_ods(str(file), sheet=sheet)
+    return Parameters(param_df)
+
+
 class Parameters:
-    def __init__(self, file: pathlib.Path, sheet: str):
-        # check if file exists
-        if not file.exists():
-            raise RuntimeError(f"Parameters file {str(file)} does not exist.")
-        # check suffix
-        if file.suffix != ".ods":
-            raise NotImplementedError(f"Detected file with suffix {file.suffix}. Only .ods files are supported.")
-        # import ods file
-        self.param_df: pd.DataFrame = read_ods(str(file), sheet=sheet)
+    def __init__(self, param_df: pd.DataFrame):
+        # set param_df as parameters dataframe
+        self.param_df: pd.DataFrame = param_df
         # set param name as index
         self.param_df.set_index("name", inplace=True)
 
