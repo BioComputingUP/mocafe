f8ebd18ca915c33480030b9196657b381f3f6c4a
francop <franco.pradelli94@gmail.com>
2021-11-17 16:53:52 +0100

    Added expression SmoothCircle

--- mocafe/expressions.py
+++ mocafe/expressions.py
@@ -4,31 +4,47 @@ import numpy as np
 from mocafe.math import perrycioc
 
 
-class SmoothCircularTumor(fenics.UserExpression):
+class SmoothCircle(fenics.UserExpression):
     """
-    Circular tumor positioned in a given center of a given radius with a smooth border
+    Circle positioned in a given center, with given values inside and outside the circle. It has a smooth border.
     """
     def __init__(self,
                  center: np.ndarray,
                  radius: float,
-                 steepness: float = 100.):
-        super(SmoothCircularTumor, self).__init__()
+                 inside_value: float,
+                 outside_value: float,
+                 slope: float = 100.):
+        super(SmoothCircle, self).__init__()
         self.center = center
         self.radius = radius
-        self.max_value = 1.
-        self.min_value = 0.
-        self.steepness = steepness
+        self.inside_value = inside_value
+        self.outside_value = outside_value
+        self.slope = slope
 
     def eval(self, values, x):
         distance_from_center = fenics.sqrt(((x[0] - self.center[0]) ** 2) + ((x[1] - self.center[1]) ** 2))
         values[0] = perrycioc(distance_from_center,
                               self.radius,
-                              self.max_value,
-                              self.min_value,
-                              self.steepness)
+                              self.inside_value,
+                              self.outside_value,
+                              self.slope)
 
     def value_shape(self):
         return ()
 
     def __floordiv__(self, other):
         pass
+
+
+class SmoothCircularTumor(SmoothCircle):
+    """
+    Circular tumor positioned in a given center of a given radius with a smooth border
+    """
+    def __init__(self,
+                 center: np.ndarray,
+                 radius: float,
+                 slope: float = 100.):
+        super(SmoothCircularTumor, self).__init__(center, radius, 1., 0., slope)
+
+    def eval(self, values, x):
+        super(SmoothCircularTumor, self).eval(values, x)
