f6cd7d5440c42df6d97ef9f48e472e81d0910858
francop <franco.pradelli94@gmail.com>
2021-12-02 14:14:00 +0100

    Added comments to base_classes.py

--- mocafe/angie/base_classes.py
+++ mocafe/angie/base_classes.py
@@ -5,10 +5,14 @@ class BaseCell:
     def __init__(self,
                  point: np.ndarray,
                  creation_step):
+        """
+        inits a BaseCell in a given point, the base class used for both source cells and tip cells
+        :param point: position of the cell
+        :param creation_step: creation step of the cell, used for internal purposes
+        """
         self.initial_position = point
         self.creation_step = creation_step
         self.position = point
-        # self.hmin = hmin
 
     def __eq__(self, other):
         return hash(other) == self.__hash__()
@@ -19,14 +23,27 @@ class BaseCell:
         return hash(id_tuple)
 
     def get_position(self):
+        """
+        get cell position
+        :return: tip cell position as numpy.ndarray
+        """
         return self.position
 
     def get_dimension(self):
+        """
+        get dimension (2D or 3D) for the cell.
+        :return: 2 for 2D, 3 for 3D
+        """
         return len(self.position)
 
     def get_distance(self, point):
-        distance_sqared = 0.
+        """
+        get the distance of the given point from the the cell
+        :param point: the point to check the distance with
+        :return: the distance
+        """
+        distance_squared = 0.
         for i in range(self.get_dimension()):
-            distance_sqared += (point[i] - self.position[i]) ** 2
-        distance = np.sqrt(distance_sqared)
+            distance_squared += (point[i] - self.position[i]) ** 2
+        distance = np.sqrt(distance_squared)
         return distance
