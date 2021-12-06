9512269b1c7e3de1fa2e47412a75e237ad0f5c12
francop <franco.pradelli94@gmail.com>
2021-12-06 10:49:04 +0100

    Documented fenut.py

--- mocafe/fenut/fenut.py
+++ mocafe/fenut/fenut.py
@@ -8,17 +8,61 @@ Requires FEniCS 2019.1 to work.
 """
 
 
-def setup_vtk_files(file_names: list, data_folder):
+def setup_pvd_files(file_names: list, data_folder):
+    """
+    returns a list of ``.pvd`` files with the given list of names. ``.pvd`` files can be used to store FEniCS Functions,
+    even though XDMF files are preferred.
+
+    This method is useful when a relative long list of files is needed for a simulation.
+
+    :param file_names: the list of strings containing the files name.
+    :param data_folder: the folder where to place the files
+    :return: the FEniCS objects representing the files.
+    """
     vtk_files = [fenics.File(str(data_folder) + "/" + file_name + ".pvd", "compressed") for file_name in file_names]
     return vtk_files
 
 
 def setup_xdmf_files(file_names: list, data_folder, comm=fenics.MPI.comm_world):
+    """
+    returns a list of ``.xdmf`` files with the given list of names. ``.xdmf`` files are the preferred way to store
+    FEniCS functions for elaboration and visualization.
+
+    This method is useful when a relative long list of files is needed for a simulation.
+
+    :param file_names: the list of strings containing the files name.
+    :param data_folder: the folder where to place the files
+    :param comm: MPI communicator. Default is COMM_WORLD and this is usually the best choice for normal simulations.
+    :return: the FEniCS objects representing the files.
+    """
     xdmf_files = [fenics.XDMFFile(comm, str(data_folder) + "/" + file_name + ".xdmf") for file_name in file_names]
     return xdmf_files
 
 
 def divide_in_chunks(given_list, n_chnks):
+    """
+    Divides the elements of a given list in ``n_chnks`` list with almost the same number of elements. The result is
+    a list of lists.
+
+    More precisely, the total number of element of the given list is divided by ``n_chnks``. If the number is not
+    divisible by ``n_chnks``, the remainder will be distributed over the lists.
+
+    Below are provided some examples:
+
+    >>> a = [1, 2, 3, 4, 5, 6, 7]
+    >>> divide_in_chunks(a, 3)
+    [[1, 2, 3], [4, 5], [6, 7]]
+
+    >>> b = [1, 2, 3, 4, 5, 6, 7, 8]
+    >>> divide_in_chunks(b, 2)
+    [[1, 2, 3, 4], [5, 6, 7, 8]]
+
+    Notice that the order is conserved.
+
+    :param given_list: the list to be divided in chunks
+    :param n_chnks: the number of desidered chunks.
+    :return: a list of lists, where each element is one of the chunks.
+    """
     quot, rem = divmod(len(given_list), n_chnks)
     chunks_list = []
     bookmark = 0
@@ -35,6 +79,11 @@ def divide_in_chunks(given_list, n_chnks):
 
 
 def load_parameters(parameters_file="parameters.json"):
+    """
+    DEPRECATED: use Parameters class instead
+    :param parameters_file:
+    :return:
+    """
     input_files_folder = "./input_files"
     with open(input_files_folder + "/" + parameters_file) as json_file:
         parameters = json.load(json_file)
@@ -44,6 +93,14 @@ def load_parameters(parameters_file="parameters.json"):
 class RectangleMeshWrapper:
     """ Wrapper for fenics.RectangleMesh with some utility method """
     def __init__(self, limit_point1: fenics.Point, limit_point2: fenics.Point, nx, ny):
+        """
+        inits a wrapper for rectangle mesh that is useful for the parallel implementation of the source cells and
+        the tip cells.
+        :param limit_point1: first point defining the rectangle
+        :param limit_point2: second point defining the rectangle
+        :param nx: number of points in the x direction
+        :param ny: number of points in the y direction
+        """
         self.local_mesh = fenics.RectangleMesh(limit_point1,
                                                limit_point2,
                                                nx, ny)
@@ -56,28 +113,58 @@ class RectangleMeshWrapper:
         self.dim = 2
 
     def get_local_mesh(self):
+        """
+        get the mesh visible to the local MPI process
+        :return: the local Mesh object
+        """
         return self.local_mesh
 
     def get_global_mesh(self):
+        """
+        get the global mesh shared among all the MPI processes
+        :return: the global mesh shared among all the MPI processes
+        """
         return self.global_mesh
 
     def is_inside_local_mesh(self, point):
+        """
+        check if the given point is inside the local mesh
+        :param point: the point to check
+        :return: True if the point is inside the local mesh. False otherwise.
+        """
         point_to_check = point if type(point) is fenics.Point else fenics.Point(point)
         return \
             self.local_bounding_box_tree.compute_first_entity_collision(point_to_check) <= self.local_mesh.num_cells()
 
     def is_inside_global_mesh(self, point):
+        """
+        check if the given point is inside the global mesh
+        :param point: the point to check
+        :return: True if the point is inside the local mesh. False otherwise
+        """
         point_to_check = point if type(point) is fenics.Point else fenics.Point(point)
         return \
             self.global_bounding_box_tree.compute_first_entity_collision(point_to_check) <= self.global_mesh.num_cells()
 
     def n_local_mesh_vertices(self):
+        """
+        get the number of points in the local mesh
+        :return: the number of points in the local mesh
+        """
         return self.local_mesh.num_vertices()
 
     def n_global_mesh_vertices(self):
+        """
+        get the number of points in the global mesh
+        :return: the number of points in the global mesh
+        """
         return self.global_mesh.num_vertices()
 
     def get_dim(self):
+        """
+        get the number of dimensions of the mesh
+        :return: 2 if the mesh is 2 dimensonal.
+        """
         return self.dim
 
 
@@ -102,6 +189,15 @@ def get_mixed_function_space(mesh: fenics.Mesh,
 
 
 def build_local_box(local_mesh, border_width):
+    """
+    Builds a local box for a given mesh.
+
+    A local box is useful for parallel computation and might be useful for different purposes. In general, defines
+    a square space, bigger than the local mesh, which may contain elements of interest for the local mesh
+    :param local_mesh: the local mesh for the current MPI process
+    :param border_width: the width of the border of the local box
+    :return: the local box
+    """
     x_list = []
     y_list = []
     for point in local_mesh.coordinates():
@@ -119,9 +215,20 @@ def build_local_box(local_mesh, border_width):
 
 
 def is_in_local_box(local_box, position):
+    """
+    Given a local box, checks if the given point is inside that local box.
+    :param local_box: the local box
+    :param position: the position to check
+    :return: True if the position is inside the local box. False otherwise
+    """
     return (local_box["x_min"] < position[0] < local_box["x_max"]) \
            and (local_box["y_min"] < position[1] < local_box["y_max"])
 
 
 def flatten_list_of_lists(list_of_lists):
+    """
+    Flattens a list of lists in a flat list
+    :param list_of_lists: the given list of lists
+    :return: the flat list
+    """
     return [elem for sublist in list_of_lists for elem in sublist]
