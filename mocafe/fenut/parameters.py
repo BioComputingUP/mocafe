3e204aae081211794d5fca96361881c039c6c760
francop <franco.pradelli94@gmail.com>
2021-12-06 11:46:49 +0100

    Documented parameters.py

--- mocafe/fenut/parameters.py
+++ mocafe/fenut/parameters.py
@@ -4,6 +4,21 @@ from pandas_ods_reader import read_ods
 
 
 def from_dict(parameters: dict):
+    """
+    Creates a mocafe Parameters object from a Python dictionary.
+
+    The structure of the Python dictionary must be::
+
+        param_dict = {
+            "name_param_a": <value_param_a>,
+            "name_param_b": <value_param_b>,
+            ...
+        }
+
+
+    :param parameters: the parameters dictionary
+    :return: the Parameters object
+    """
     param_df = pd.DataFrame({
         "name": parameters.keys(),
         "sim_value": [parameters[param] for param in parameters]
@@ -12,6 +27,18 @@ def from_dict(parameters: dict):
 
 
 def from_ods_sheet(file: pathlib.Path, sheet: str):
+    """
+    Creates a mocafe Parameters object form a sheet of an ``.odf`` file.
+
+    The given sheet must have at least to columns: ``name``, with the parameters' names; and ``sim_value``, with the
+    value of the parameters. Any other column, for instance containing the measure units, the reference of the
+    parameters, or other informations, will be stored inside the Parameters object and used for reporting informations,
+    but are not compulsory.
+
+    :param file: file path
+    :param sheet: sheet of the file to be used to load parameters
+    :return: the Parameters object
+    """
     # check if file exists
     if not file.exists():
         raise RuntimeError(f"Parameters file {str(file)} does not exist.")
@@ -24,17 +51,46 @@ def from_ods_sheet(file: pathlib.Path, sheet: str):
 
 
 class Parameters:
+    """
+    Class representing the simulation parameters.
+
+    It is basically a wrapper for a pandas.DataFrame, which contains everything about the simulation parameters.
+    """
     def __init__(self, param_df: pd.DataFrame):
+        """
+        inits a Parameters object from a given ``pandas.DataFrame`` which contains the names and the values of the
+        parameters.
+
+        :param param_df: the ``pandas.DataFrame`` containing the parameters names and values.
+        """
         # set param_df as parameters dataframe
         self.param_df: pd.DataFrame = param_df
         # set param name as index
         self.param_df.set_index("name", inplace=True)
 
     def get_value(self, name: str):
+        """
+        Get the parameter value from the given name.
+
+        :param name: parameter name
+        :return: the value of the parameter with the given name.
+        """
         return self.param_df.loc[name, "sim_value"]
 
     def set_value(self, name: str, new_value):
+        """
+        Set a value for the parameter of the given name.
+
+        :param name: parameter name
+        :param new_value: the new value for the parameter
+        :return:
+        """
         self.param_df.loc[name, "sim_value"] = new_value
 
     def as_dataframe(self):
+        """
+        Get the ``pandas.DataFrame`` representing the Parameters object
+
+        :return: the dataframe representing the Parameters object
+        """
         return self.param_df
