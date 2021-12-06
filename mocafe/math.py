0f00d5711636f79546e7bd1a83a3583ffb559fc7
francop <franco.pradelli94@gmail.com>
2021-12-06 14:48:49 +0100

    Documented math.py

--- mocafe/math.py
+++ mocafe/math.py
@@ -1,17 +1,24 @@
 import fenics
 
 
-def perrycioc(variable,
-              c: float,
-              a: float,
-              b: float,
-              slope: float):
-    """
-    Sigmoid function as formalized by J.W.Perry on StackExchange1 [1].
-    References:
-     - [1] https://math.stackexchange.com/questions/535860/what-is-the-simplest-formula-for-activation-smooth-step-function
-    :param variable: variable along which the sigmoid will be defined
-    :param c: value of the variable for which the derivative of the sigmoid is max
+def sigmoid(variable,
+            c: float,
+            a: float,
+            b: float,
+            slope: float):
+    r"""
+    Simple sigmoid function defined as:
+
+    .. math::
+        y(x) = \frac{a \cdot e^{slope \cdot c} + b \cdot e^{r \cdot x}}{e^{slope \cdot c} + e^{slope \cdot c}}
+
+    Where: :math:`a` represent the value of the sigmoid for x -> -Inf; :math:`b` represent the value of the sigmoid
+    for x -> +Inf; :math:`c` represent the center of the sigmoid (where the derivative is max); .math:`slope`
+    represent the slope of the sigmoid.
+
+
+    :param variable: variable x for which the sigmoid is defined
+    :param c: central value of the sigmoid
     :param a: value of the sigmoid for variable -> -inf
     :param b: value of the sigmoid for variable -> +inf
     :param slope: slope of the sigmoid
@@ -22,20 +29,25 @@ def perrycioc(variable,
 
 
 def shf(variable, slope: float = 100):
-    """
-    Smoothed Heavyside Function (SHF) using the sigmoid function, that is:
-            exp(slope * variable) / (1 + exp(slope * variable))
-    :param variable: variable along which a step must be considered
-    :param slope: slope of the SHF (100 by default)
-    :return: symbolic form of the SHF compatible with FEniCS Form definition
+    r"""
+    Smoothed Heavyside Function (SHF) using the sigmoid function, which reads:
+
+    .. math::
+        \frac{e^{slope * variable}}{(1 + e^{slope * variable})}
+
+
+    :param variable: varible for the SHF
+    :param slope: slope of the SHF. Default is 100
+    :return: the value of the sigmoid for the given value of the variable
     """
     return fenics.exp(slope * variable) / (1 + fenics.exp(slope * variable))
 
 
 def estimate_cancer_area(phi: fenics.Function):
-    """
-    Estimate cancer area for the phase field phi representing the cancer.
-    In order to work phi must be between 0 and 1.
+    r"""
+    Estimate cancer area for the phase field :math:`\varphi` representing the cancer, where the phase field must
+    have values between 0 and 1
+
     :param phi: phase field representing the cancer
     :return: the area of the given cancer
     """
@@ -45,8 +57,9 @@ def estimate_cancer_area(phi: fenics.Function):
 def estimate_capillaries_area(c: fenics.Function,
                               V: fenics.FunctionSpace):
     """
-    Estimate capillaries area for the phase field c representing the vessels.
-    In order to work c must be between -1 and 1
+    Estimate capillaries area for the phase field c representing the vessels. In order to work c must be
+    between -1 and 1.
+
     :param c: phase field representing capillaries
     :param V: function space of c
     :return: the area of the given field
@@ -54,7 +67,7 @@ def estimate_capillaries_area(c: fenics.Function,
     # rescale phase field c
     c_rescaled = fenics.project(
         fenics.Constant(1) + (fenics.Constant(0.5) * c),
-        V
+        c.function_space
     )
     # estimate area of the rescaled field
     return estimate_cancer_area(c_rescaled)
