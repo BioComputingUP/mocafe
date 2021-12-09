73f03393e1d953c8e5226dd531e89f89f27d06c5
francop <franco.pradelli94@gmail.com>
2021-12-09 17:40:25 +0100

    Modified setup_data_folder to make it easier to use

--- test/fenut/unit_test/mansimdata/test_setup_folder.py
+++ test/fenut/unit_test/mansimdata/test_setup_folder.py
@@ -21,13 +21,12 @@ def get_p0_tmpdir(tmpdir):
 
 def test_setup_data_folder(tmpdir):
     # setup data_folder
-    data_folder = mansim.setup_data_folder(mansim.test_sim_name, base_location=tmpdir)
+    data_folder = mansim.setup_data_folder(folder_name=mansim.test_sim_name, base_location=tmpdir)
     # test if exists
     assert data_folder.exists(), "The folder should exist"
     # test if the name is the one expected
     p0_tmpdir = get_p0_tmpdir(tmpdir)
-    assert str(data_folder) == f"{p0_tmpdir}/{mansim.default_saved_sim_folder_name}/{mansim.test_sim_name}/0000", \
-        f"The folder name should be {p0_tmpdir}/saved_sim/test/0000"
+    assert str(data_folder) == f"{p0_tmpdir}/{mansim.test_sim_name}/0000"
 
 
 def test_setup_multiple_data_folder(tmpdir):
@@ -36,19 +35,22 @@ def test_setup_multiple_data_folder(tmpdir):
     data_folder2 = mansim.setup_data_folder(mansim.test_sim_name, base_location=tmpdir)
 
     assert data_folder1.exists(), "Data folder 1 should exist"
-    assert str(data_folder1) == f"{get_p0_tmpdir(tmpdir)}/{mansim.default_saved_sim_folder_name}" \
+    assert str(data_folder1) == f"{get_p0_tmpdir(tmpdir)}" \
                                 f"/{mansim.test_sim_name}/0000", \
-        f"The data folder 1 should be {get_p0_tmpdir(tmpdir)}/{mansim.default_saved_sim_folder_name}" \
+        f"The data folder 1 should be {get_p0_tmpdir(tmpdir)}" \
                                 f"/{mansim.test_sim_name}/0000"
     assert data_folder2.exists(), "Data folder 2 should exist"
-    assert str(data_folder2) == f"{get_p0_tmpdir(tmpdir)}/{mansim.default_saved_sim_folder_name}" \
+    assert str(data_folder2) == f"{get_p0_tmpdir(tmpdir)}" \
                                 f"/{mansim.test_sim_name}/0001", \
-        f"The data folder 2 should be {get_p0_tmpdir(tmpdir)}/{mansim.default_saved_sim_folder_name}" \
+        f"The data folder 2 should be {get_p0_tmpdir(tmpdir)}" \
                                 f"/{mansim.test_sim_name}/0001"
 
 
-def test_setup_runtime(tmpdir):
-    data_folder = mansim.setup_data_folder(base_location=tmpdir)
-    assert data_folder.exists() \
-           and str(data_folder) == f"{get_p0_tmpdir(tmpdir)}/{mansim.default_runtime_folder_name}", \
-           "The folder should be 'tmpdir/runtime/'"
+def test_setup_multiple_data_folder_no_enumerate(tmpdir):
+    # define multiple data folders
+    data_folder1 = mansim.setup_data_folder(mansim.test_sim_name, base_location=tmpdir, enumerate=False)
+    data_folder2 = mansim.setup_data_folder(mansim.test_sim_name, base_location=tmpdir, enumerate=False)
+
+    assert data_folder1.exists(), "Data folder 1 should exist"
+    assert data_folder2.exists(), "Data folder 2 should exist"
+    assert str(data_folder1) == str(data_folder2), "The two should be equal"
