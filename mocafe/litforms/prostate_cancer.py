776b7899d7f17cc422446e019d65165d86a939b3
francop <franco.pradelli94@gmail.com>
2021-12-09 12:40:27 +0100

    Small fix to prostate_cancer.py

--- mocafe/litforms/prostate_cancer.py
+++ mocafe/litforms/prostate_cancer.py
@@ -2,20 +2,16 @@ import fenics
 import mocafe.fenut.parameters as mcfp
 
 
-def prostate_cancer_chem_potential(phi: fenics.Function,
+def prostate_cancer_chem_potential(var_phi: fenics.Variable,
                                    chem_potential_constant):
-    var_phi = fenics.variable(phi)
     chem_potential = chem_potential_constant * (var_phi ** 2) * ((1 - var_phi) ** 2)
     return chem_potential
 
 
 def df_dphi(phi, chem_potential_constant):
-    # var_phi = fenics.variable(phi)
-    # c1 = potential_constant * 2
-    # c2 = - potential_constant * 6
-    # c3 = potential_constant * 4
-    chem_potential = prostate_cancer_chem_potential(phi, chem_potential_constant)
-    return fenics.diff(chem_potential)
+    var_phi = fenics.variable(phi)
+    chem_potential = prostate_cancer_chem_potential(var_phi, chem_potential_constant)
+    return fenics.diff(chem_potential, var_phi)
 
 
 def prostate_cancer_form(phi: fenics.Function,
@@ -38,9 +34,10 @@ def prostate_cancer_nutrient_form(sigma: fenics.Function,
                                   v: fenics.TestFunction,
                                   s_expression: fenics.Expression,
                                   parameters: mcfp.Parameters):
+    s = fenics.interpolate(s_expression, sigma_old.function_space())
     F = (((sigma - sigma_old) / parameters.get_value("dt")) * v * fenics.dx) \
         + (parameters.get_value("epsilon") * fenics.dot(fenics.grad(sigma), fenics.grad(v)) * fenics.dx) \
-        + (- s_expression * v * fenics.dx) \
+        + (- s * v * fenics.dx) \
         + (parameters.get_value("delta") * phi * v * fenics.dx) \
         + (parameters.get_value("gamma") * sigma * v * fenics.dx)
 
