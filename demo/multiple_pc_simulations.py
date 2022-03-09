3790491fbd8b2b49e7909c8617c8bcbc311ccb31
francop <franco.pradelli94@gmail.com>
2022-03-09 17:36:37 +0100

    Changed scale in the image of multiple_pc_simulations.py

--- demo/multiple_pc_simulations.py
+++ demo/multiple_pc_simulations.py
@@ -131,7 +131,7 @@ you can easly import the ``.xdmf`` files generated during the simulation and vis
 #   For instance, this is the content of the file generated for the first simulation:
 #
 # .. image:: demo_out/multiple_pc_simulations/0000/sim_info.png
-#   :width: 600
+#    :scale: 50 %
 #
 # After the loop, the result will be stored in a tree like the following:
 #
