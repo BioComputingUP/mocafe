80e49c43a4a8a1427e5eb6ca300d502b25f9ed82
francop <franco.pradelli94@gmail.com>
2022-05-16 18:11:08 +0200

    Updated mocafe docstring

--- mocafe/__init__.py
+++ mocafe/__init__.py
@@ -5,6 +5,9 @@ Mocafe (modeling cancer with FEniCS) is composed of different subpackages:
   al. (2011) :cite:`Travasso2011a`
 * ``fenut`` contains general-purpose modules and classes
 * ``litforms`` contains the implementation of other Phase Field Models presented to literature
+
+And submodules:
+
 * ``expressions`` contains useful fenics Expression for cancer Phase Field Models
 * ``math`` contains useful mathematical functions for cancer Phase Field Models
 
