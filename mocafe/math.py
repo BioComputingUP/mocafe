3f16d04d18829fef84eb20a7ac16c12593c618e8
francop <franco.pradelli94@gmail.com>
2022-05-19 11:52:45 +0200

    Modified integration function, added project function

--- mocafe/math.py
+++ mocafe/math.py
@@ -1,4 +1,7 @@
-import fenics
+import ufl
+import dolfinx
+from mpi4py import MPI
+from petsc4py import PETSc
 
 
 def sigmoid(variable,
@@ -24,8 +27,8 @@ def sigmoid(variable,
     :param slope: slope of the sigmoid
     :return: the value of the sigmoid for the given value of the variable
     """
-    return ((a * fenics.exp(c * slope)) + (b * fenics.exp(slope * variable))) / \
-           (fenics.exp(c * slope) + fenics.exp(slope * variable))
+    return ((a * ufl.exp(c * slope)) + (b * ufl.exp(slope * variable))) / \
+           (ufl.exp(c * slope) + ufl.exp(slope * variable))
 
 
 def shf(variable, slope: float = 100):
@@ -40,22 +43,28 @@ def shf(variable, slope: float = 100):
     :param slope: slope of the SHF. Default is 100
     :return: the value of the sigmoid for the given value of the variable
     """
-    return fenics.exp(slope * variable) / (1 + fenics.exp(slope * variable))
+    return ufl.exp(slope * variable) / (1 + ufl.exp(slope * variable))
 
 
-def estimate_cancer_area(phi: fenics.Function):
+def estimate_field01_integral(phi: dolfinx.fem.Function):
     r"""
-    Estimate cancer area for the phase field :math:`\varphi` representing the cancer, where the phase field must
+    Estimate phase field area for the input function, where the phase field must
     have values between 0 and 1
 
     :param phi: phase field representing the cancer
     :return: the area of the given cancer
     """
-    return fenics.assemble(phi * fenics.dx)
+    # compute integral (only local on dolfinx)
+    local_integral = dolfinx.fem.assemble_scalar(dolfinx.fem.form(phi * ufl.dx))
+    # sum across all MPI domain and store the result on 0
+    total_integral = MPI.COMM_WORLD.reduce(local_integral, op=MPI.SUM, root=0)
+    # broadcast to all processes
+    total_integral = MPI.COMM_WORLD.bcast(total_integral, root=0)
+    return total_integral
 
 
-def estimate_capillaries_area(c: fenics.Function,
-                              V: fenics.FunctionSpace):
+def estimate_capillaries_area(c: dolfinx.fem.Function,
+                              V: dolfinx.fem.FunctionSpace):
     """
     Estimate capillaries area for the phase field c representing the vessels. In order to work c must be
     between -1 and 1.
@@ -65,9 +74,44 @@ def estimate_capillaries_area(c: fenics.Function,
     :return: the area of the given field
     """
     # rescale phase field c
-    c_rescaled = fenics.project(
-        fenics.Constant(1) + (fenics.Constant(0.5) * c),
-        c.function_space
-    )
+    c_rescaled = dolfinx.fem.Function(V)
+    c_rescaled.vector.array[:] = (c.vector.array + 1.) / 2
+    # estimate area
+    area = estimate_field01_integral(c_rescaled)
     # estimate area of the rescaled field
-    return estimate_cancer_area(c_rescaled)
+    return area
+
+
+def project(v, target_func, bcs=[]):
+    """
+    Project a given form and store the result on the target function.
+
+    Thanks to @michalhabera for the implementation.
+
+    :param v: the form to be projected
+    :param target_func: the function where the result will be stored
+    :param bcs: boundary conditions, if any
+    :return: nothing
+    """
+    # Ensure we have a mesh and attach to measure
+    V = target_func.function_space
+    dx = ufl.dx(V.mesh)
+
+    # Define variational problem for projection
+    w = ufl.TestFunction(V)
+    Pv = ufl.TrialFunction(V)
+    a = dolfinx.fem.form(ufl.inner(Pv, w) * dx)
+    L = dolfinx.fem.form(ufl.inner(v, w) * dx)
+
+    # Assemble linear system
+    A = dolfinx.fem.petsc.assemble_matrix(a, bcs)
+    A.assemble()
+    b = dolfinx.fem.petsc.assemble_vector(L)
+    dolfinx.fem.petsc.apply_lifting(b, [a], [bcs])
+    b.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
+    dolfinx.fem.petsc.set_bc(b, bcs)
+
+    # Solve linear system
+    solver = PETSc.KSP().create(A.getComm())
+    solver.setOperators(A)
+    solver.solve(b, target_func.vector)
---
+++ test/math/test_math.py
@@ -0,0 +1,20 @@
+import dolfinx
+import numpy as np
+from mpi4py import MPI
+from mocafe.math import estimate_field01_integral
+
+_comm = MPI.COMM_WORLD
+
+
+def test_estimate_field01_integral():
+    # create a unit square mesh
+    mesh = dolfinx.mesh.create_unit_square(_comm, 10, 10)
+    # define function space
+    V = dolfinx.fem.FunctionSpace(mesh, ("CG", 1))
+    # define a constant function on it
+    constant_function = dolfinx.fem.Function(V)
+    constant_function.interpolate(lambda x: np.ones(x.shape[1]))
+    # compute the integral
+    integral = estimate_field01_integral(constant_function)
+    # should be one
+    assert np.isclose(integral, 1.), "It should be 1."
