dc8ffbf9eea7d1125601f052dfa34ff23ebb212f
fpradelli94 <franco.pradelli94@gmail.com>
2023-02-28 09:58:33 -0300

    Added test on setup_xdmf_files

--- test/fenut/unit_test/mansimdata/test_setup_folder.py
+++ test/fenut/unit_test/mansimdata/test_setup_folder.py
@@ -5,44 +5,28 @@ comm = fenics.MPI.comm_world
 rank = comm.Get_rank()
 
 
-def get_p0_tmpdir(tmpdir):
-    """
-    Utility method to get tmpdir from MPI process 0 and broadcast it to all processes
-    :param tmpdir: current process tmpdir
-    :return: the tmpdir for MPI process 0
-    """
-    if rank == 0:
-        p0_tmpdir = tmpdir
-    else:
-        p0_tmpdir = None
-    p0_tmpdir = comm.bcast(p0_tmpdir, 0)
-    return p0_tmpdir
-
-
-def test_setup_data_folder(tmpdir):
+def test_setup_data_folder(tmpdir, get_p0_tmpdir):
     # setup data_folder
     data_folder = mansim.setup_data_folder(folder_path=f"{tmpdir}/{mansim.test_sim_name}")
     # test if exists
     assert data_folder.exists(), "The folder should exist"
-    # test if the name is the one expected
-    p0_tmpdir = get_p0_tmpdir(tmpdir)
-    assert str(data_folder) == f"{p0_tmpdir}/{mansim.test_sim_name}/0000"
+    assert str(data_folder) == f"{get_p0_tmpdir}/{mansim.test_sim_name}/0000"
 
 
-def test_setup_multiple_data_folder(tmpdir):
+def test_setup_multiple_data_folder(tmpdir, get_p0_tmpdir):
     # define multiple data folders
     data_folder1 = mansim.setup_data_folder(folder_path=f"{tmpdir}/{mansim.test_sim_name}")
     data_folder2 = mansim.setup_data_folder(folder_path=f"{tmpdir}/{mansim.test_sim_name}")
 
     assert data_folder1.exists(), "Data folder 1 should exist"
-    assert str(data_folder1) == f"{get_p0_tmpdir(tmpdir)}" \
+    assert str(data_folder1) == f"{get_p0_tmpdir}" \
                                 f"/{mansim.test_sim_name}/0000", \
-        f"The data folder 1 should be {get_p0_tmpdir(tmpdir)}" \
+        f"The data folder 1 should be {get_p0_tmpdir}" \
                                 f"/{mansim.test_sim_name}/0000"
     assert data_folder2.exists(), "Data folder 2 should exist"
-    assert str(data_folder2) == f"{get_p0_tmpdir(tmpdir)}" \
+    assert str(data_folder2) == f"{get_p0_tmpdir}" \
                                 f"/{mansim.test_sim_name}/0001", \
-        f"The data folder 2 should be {get_p0_tmpdir(tmpdir)}" \
+        f"The data folder 2 should be {get_p0_tmpdir}" \
                                 f"/{mansim.test_sim_name}/0001"
 
 
